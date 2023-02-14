06e5eed7670fd1bc2fb99d255ef430046f0aeb77
Uxass <babusenko2004@mail.ru>
2023-02-14 13:32:22 +0300

    1

---
+++ 1lab/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Домашняя работа</title>
+</head>
+<body>
+    <header>
+        <div class = "block">
+            <img src="icon.png">
+            <h1>Hello, world!</h1>
+        <div>
+    </header>
+    <main>
+        <?php 
+        echo  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet tellus purus. Praesent nec tempus justo. Pellentesque tincidunt, quam vel faucibus viverra, elit est efficitur massa, at volutpat nulla.';
+        ?>
+    </main>
+    <footer>
+        
+    </footer>
+</body>
+</html>
